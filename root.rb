0b52a09587207e35be34e4c00b31a90eda6d7145
Feifan Zhou <feifan@me.com>
2015-05-22 17:59:49 -0400

    Stub in WebSocket client support
    Use WebSockets for real-time Slack API. Needed to get messages out of
    private groups, might use WebSockets to post messages as well.
    Ref T95

--- root.rb
+++ root.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'slack'
 
 get '/' do
   "Server has not crashed"
+end
+
+Slack.configure do |config|
+  config.token = 'YOUR_TOKEN'
+end
+
+EM.run do
+  ws = WebSocket::EventMachine::Client.connect(uri: 'uri')
+
+  ws.onmessage do |message, type|
+  end
+
+  ws.onerror do |error|
+  end
+
+  ws.onclose do |code, reason|
+  end
 end
