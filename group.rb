83d8d9d54bccba615d459336711b6c6133054582
Feifan Zhou <feifan@me.com>
2015-05-22 18:13:36 -0400

    Functionality for working with groups through HTTP API
    Ref T95
    Ref T119
    Ref T115
    Ref T114

---
+++ group.rb
@@ -0,0 +1,48 @@
+require 'slack'
+
+class Group
+  @@cached_groups = {}
+
+  attr_reader :id, :name
+  def initialize(attributes)
+    @id = attributes['id']
+    @name = attributes['id']
+  end
+
+  def invite(user)
+    options = {
+      user: (user.is_a? User) ? user.id : user
+    }
+    Slack.groups_invite(group_options.merge(options))
+    # TODO: Check response
+  end
+
+  def archive
+    Slack.groups_archive(group_options)
+  end
+
+  def self.create(name)
+    options = {
+      name: name
+    }
+    resp = Slack.groups_create(options)
+    group = Group.new(resp['group'])
+    @@cached_groups[group.name] = group
+  end
+
+  private
+  def self.load_list
+    resp = Slack.groups_list
+    # TODO: Check 'ok' field for error
+    resp['groups'].each do |group_json|
+      group = Group.new(group_json)
+      @@cached_groups[group.name] = group
+    end
+  end
+
+  def group_options
+    {
+      channel: self.id
+    }
+  end
+end
