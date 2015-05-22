876383de53446597cf6beb62548d8af66c280ba5
Feifan Zhou <feifan@me.com>
2015-05-22 18:27:45 -0400

    Populate users and find by email
    Useful when authenticating/posting as a particular user
    
    Ref T95
    Ref T118
    Ref T117
    Ref T116
    Ref T115

---
+++ user.rb
@@ -0,0 +1,28 @@
+class User
+  @@cached_users = {}
+
+  attr_reader :id, :user_name, :first_name, :last_name, :email
+  def initialize(attributes)
+    @id = attributes['id']
+    @user_name = attributes['name']
+    profile = attributes['profile']
+    @first_name = profile['first_name']
+    @last_name = profile['last_name']
+    @email = profile['email']
+  end
+
+  def self.find_by_email(email)
+    User.load_list unless @@cached_users[email]
+    @@cached_users[email]
+  end
+
+  private
+  def self.load_list
+    resp = Slack.users_list
+    resp['members'].each do |user_json|
+      next if user_json['deleted']
+      user = User.new(user_json)
+      @@cached_users[user.email] = user
+    end
+  end
+end
