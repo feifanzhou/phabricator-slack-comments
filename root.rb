33e794081d9919e68b84299a36cbcd9629e048bb
Feifan Zhou <feifan@me.com>
2015-05-28 10:48:57 -0400

    Add POST endpoint for Phabricator to send feed activity to
    Ref T95
    Ref T117

--- root.rb
+++ root.rb
@@ -5,6 +5,12 @@ get '/' do
   "Server has not crashed"
 end
 
+post '/feed' do
+  p '========================'
+  p 'Post feed'
+  p params
+end
+
 Slack.configure do |config|
-  config.token = 'YOUR_TOKEN'
+  config.token = ENV['SENDO_API_TOKEN']
 end
